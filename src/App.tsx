c16717d39299fa2bfbe34670409c7934f63dfae9
kermkaze <ukojielvis@gmail.com>
2025-09-07 21:19:41 +0000

    feat(GH-#6): assemble components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { TaskForm } from "./components/taskform/TaskForm.tsx";
+import { TaskList } from "./components/tasklist/TaskList.tsx";
+import { useState } from "react";
+import { useTasks } from "./hooks/useTasks.ts";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showForm, setShowForm] = useState(false);
+
+  const handleAddTask = () => setShowForm(true);
+  const handleCloseForm = () => setShowForm(false);
+
+  const { tasks, filterApplied } = useTasks();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={"app"}>
+      <h1>Task Tracker</h1>
+      <TaskList
+        tasks={tasks}
+        filterApplied={filterApplied}
+        onAddTask={handleAddTask}
+        showAddButton={!showForm}
+      />
+      {showForm && <TaskForm onClose={handleCloseForm} />}
+    </div>
+  );
 }
 
-export default App
+export default App;
